df7674904ec703c01b3d328c66c149d61dacdf19
CaptainBern <captainberneby@gmail.com>
2014-09-16 17:00:20 +0200

    Changed pom + added maven jar plugin

--- src/main/java/io/github/ledge/client/LedgeClient.java
+++ src/main/java/io/github/ledge/client/LedgeClient.java
@@ -4,14 +4,21 @@ import io.github.ledge.engine.GameRegistry;
 import io.github.ledge.engine.LedgeEngine;
 import io.github.ledge.engine.LedgeTiming;
 import io.github.ledge.engine.tick.Timing;
+import io.github.ledge.utils.LwjglUtils;
 
 public class LedgeClient {
 
     public static void main(String[] args) {
-        GameRegistry.register(Timing.class, new LedgeTiming());
+        try {
+            LwjglUtils.intializeLwjgl();
 
-        LedgeEngine engine = new LedgeEngine();
+            GameRegistry.register(Timing.class, new LedgeTiming());
 
-        engine.run(new TestState());
+            LedgeEngine engine = new LedgeEngine();
+
+            engine.run(new TestState());
+        } catch (Exception e) {
+
+        }
     }
 }
